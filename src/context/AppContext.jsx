5fa79c430afb2b19117702708eb1afb14fc76e8a
Clausemberg Rodrigues de Oliveira <clausemberg@yahoo.com.br>
2026-05-19 15:43:58 +0000

    Generated by Spark: Fix all reported errors.

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,6 +3,8 @@ import { initialData } from '../data/mock';
 
 const AppContext = createContext(null);
 
+export { AppContext };
+
 export const useApp = () => {
   const context = useContext(AppContext);
   if (!context) throw new Error('useApp must be used within AppProvider');
